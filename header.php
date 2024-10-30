b4174e256fa8e124d20c69d9177d3659e1e84493
Jalila Afkir <larmachjalila69@gmail.com>
2024-10-30 20:54:34 +0100

    Header et footer

---
+++ header.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The header for our theme
+ * 
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Photographe event - Nathalie Mota
+*/
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?> >
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="profile" href="https://gmpg.org/xfn/11">
+	<meta name="keywords" content="Nathalie Mota, photographe event, format HD, photographe professionnelle dans l’événementiel"/>
+	<meta name="description" content="Photographe event - Nathalie Mota - Projet 11 : site WordPress complexe et sur mesure pour une photographe."/>
+    
+    <?php wp_head(); ?>
+</head>
